7282390054f1d58ee6cfedec25437d7161cdc20e
unit-kin <akkangara8@gmail.com>
2023-02-18 09:08:03 -0800

    0x01 corrections

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/**
- * main - main block
- * Description: Get a random number and print the number
- * and if it is positive, negative, or zero
- * Return: 0
- */
+
 int main(void)
 {
-	int n;
+	int r;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	r = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (r > 0)
 		printf("%i is positive\n", n);
-	else if (n < 0)
+	else if (r < 0)
 		printf("%i is negative\n", n);
 	else
 		printf("%i is zero\n", n);
--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
-/**
- * main - main block
- * Description: Print all possible combinations of two digits.
- * Numbers must be separated by commas and a space.
- * 01 and 10 are considered as the same combination of the two digits.
- * Print only the smallest combination of two digits.
- * Numbers should be printed in ascending order, with two digits.
- * You can only use `putchar`.
- * You can only use `putchar` up to 5 times.
- * You are not allowed to use any variable of type `char`.
- * Return: 0
- */
 int main(void)
 {
-	int i, j, k;
+	int a, b, c;
 
-	i = 0;
+	a = 0;
 
-	while (i < 100)
+	while (a < 100)
 	{
-		j = i % 10; /* singles digit */
-		k = i / 10; /* doubles digit */
+		b = a % 10; 
+		c = a / 10; 
 
-		if (k < j)
+		if (c < b)
 		{
-			putchar(k + '0');
-			putchar(j + '0');
+			putchar(c + '0');
+			putchar(b + '0');
 
-			if (i < 89)
+			if (a < 89)
 			{
 				putchar(44);
 				putchar(32);
 			}
 		}
 
-		i++;
+		a++;
 	}
 	putchar('\n');
 
