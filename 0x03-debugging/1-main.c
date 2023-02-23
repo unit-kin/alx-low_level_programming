0b6094cb905e12a05b93d85ccb2e13971d862b37
unit-kin <akkangara8@gmail.com>
2023-02-23 14:37:48 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
+	i = 0;
 	/*
-	 * i = 0;
-	 *
-	 * while(i < 10)
-	 * {
-	 * putchr(i);
-	 * }
-	 */
+	* while(i < 10)
+	* {
+	* putchr(i);
+	* }
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
