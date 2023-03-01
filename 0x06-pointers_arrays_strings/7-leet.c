5361b6ccc2a6b904626f966fcd2e922fd6c77316
unit-kin <akkangara8@gmail.com>
2023-03-01 13:25:17 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,20 +6,24 @@
  * Return: encoded string `s`
  */
 
-char *leet(char *s) {
-    int i, j;
-    char leet_letters[] = {'4', '3', '0', '7', '1'};
-    char normal_letters[] = {'a', 'e', 'o', 't', 'l'};
+char *leet(char *s)
+{
+	int i, j;
+	char leet_letters[] = {'4', '3', '0', '7', '1'};
+	char normal_letters[] = {'a', 'e', 'o', 't', 'l'};
 
-    for (i = 0; s[i] != '\0'; i++) {
-        for (j = 0; j < 5; j++) {
-            if (s[i] == normal_letters[j] || s[i] == normal_letters[j] - 'a' + 'A') {
-		    s[i] = leet_letters[j];
-		    break;
-	    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == normal_letters[j] || s[i] == normal_letters[j] - 'a' + 'A')
+			{
+				s[i] = leet_letters[j];
+				break;
+			}
+		}
 	}
-    }
 
-    return s;
+	return (s);
 }
 
