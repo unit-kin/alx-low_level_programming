cd0db9e71be4609a66ad77eebf420398d7ac6a84
unit-kin <akkangara8@gmail.com>
2023-03-01 13:11:36 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,19 +10,25 @@ char *leet(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+	while (s[i] != '\0') {
+        if (s[i] == 'a' || s[i] == 'A') {
+            s[i] = '4';
+        }
+        else if (s[i] == 'e' || s[i] == 'E') {
+            s[i] = '3';
+        }
+        else if (s[i] == 'o' || s[i] == 'O') {
+            s[i] = '0';
+        }
+        else if (s[i] == 't' || s[i] == 'T') {
+            s[i] = '7';
+        }
+        else if (s[i] == 'l' || s[i] == 'L') {
+            s[i] = '1';
+        }
+        i++;
 	}
+
 
 	return (s);
 }
