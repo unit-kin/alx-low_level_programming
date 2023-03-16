c7de38bdedb056da75780c1a50690bc3a42d7d73
unit-kin <akkangara8@gmail.com>
2023-03-16 12:25:23 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,113 +1,68 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * is_digit - Check if a string is a digit
- *
- * @s: String to check
- *
- * Return: 1 if the string is a digit, 0 otherwise
+ * _isdigit - tells if the string consists of digits
+ * @argv: pointer to current item in argument
+ * Return: return 0 if all digits, 1 if not all digits.
  */
-int is_digit(char *s)
+int _isdigit(char *argv)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (argv[i])
 	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
+		if (argv[i] >= '0' && argv[i] <= '9')
+			i++;
+		else
+			return (1);
 	}
-
-	return (1);
+	return (0);
 }
-
 /**
- * mul - Multiply two numbers
- *
- * @num1: First number to multiply
- * @num2: Second number to multiply
- *
- * Return: The result of the multiplication
+ * _atoi - converts a string of ascii digits to the values they represent
+ * @s: pointer to the source string
+ * Return: value of digits
  */
-char *mul(char *num1, char *num2)
+int _atoi(char *s)
 {
-	int len1 = 0, len2 = 0, i, j, k, carry = 0, n1, n2, sum;
-	char *result;
+	int i, result;
 
-	/* Get the length of each number */
-	while (num1[len1] != '\0')
-		len1++;
-	while (num2[len2] != '\0')
-		len2++;
-
-	/* Allocate memory for the result */
-	result = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (result == NULL)
-		return (NULL);
-
-	/* Initialize the result */
-	for (i = 0; i < len1 + len2; i++)
-		result[i] = '0';
-	result[len1 + len2] = '\0';
-
-	/* Multiply each digit of num2 with num1 */
-	for (i = len2 - 1; i >= 0; i--)
+	i = result = 0;
+	while (s[i])
 	{
-		carry = 0;
-		n2 = num2[i] - '0';
-
-		/* Multiply each digit of num1 with the current digit of num2 */
-		for (j = len1 - 1, k = len1 + i; j >= 0; j--, k--)
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			n1 = num1[j] - '0';
-			sum = n1 * n2 + carry + (result[k] - '0');
-			carry = sum / 10;
-			result[k] = (sum % 10) + '0';
+			result *= 10;
+			result += (s[i] - '0');
 		}
-
-		/* Add the carry to the remaining digits in the result */
-		if (carry > 0)
-		{
-			sum = carry + (result[k] - '0');
-			carry = sum / 10;
-			result[k] = (sum % 10) + '0';
-			k--;
-		}
+		i++;
 	}
-
-	/* Remove leading zeros */
-	for (i = 0; result[i] == '0' && result[i + 1] != '\0'; i++)
-		;
-	if (i > 0)
-	{
-		for (j = i; j <= len1 + len2; j++)
-			result[j - i] = result[j];
-	}
-
 	return (result);
 }
-
 /**
- * main - Entry point
- *
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * Return: 0 on success, 98 on failure
+ * main - main function call
+ * @argc: argument count
+ * @argv: 2D array of arguments
+ * Return: return 0 on success, 98 on failure
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *num1, *num2, *result;
+	int i;
 
-	/* Check the number of arguments */
+	malloc();
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
-
-	/* Check if the arguments are digits */
-	num1 = argv[1];
-	num2 = argv[2];
-	if (!is_digit(num1
-
+	for (i = 1; i < argc; i++)
+	{
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	return (0);
+}
