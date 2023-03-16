00fa62b25aa2ac11ca64c7d32514d9fe2a0e04f0
unit-kin <akkangara8@gmail.com>
2023-03-16 12:07:06 -0700

    corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,41 +3,39 @@
 #include <string.h>
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: the first string
- * @s2: the second string
- * @n: the maximum number of bytes from s2 to concatenate
+ * string_nconcat - concatenates two strings, up to n bytes from s2
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to concatenate from s2
  *
- * Return: a pointer to the newly allocated space in memory, or NULL if it fails
+ * Return: pointer to newly allocated space containing s1 followed by
+ * the first n bytes of s2, null terminated, or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, i;
-	char *str;
+	char *new_str;
+	unsigned int s1_len = 0, s2_len = 0, i, j;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (s1 != NULL)
+		s1_len = _strlen(s1);
+	if (s2 != NULL)
+		s2_len = _strlen(s2);
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	if (n >= s2_len)
+		n = s2_len;
 
-	if (n >= len2)
-		n = len2;
-
-	str = malloc(len1 + n + 1);
-	if (str == NULL)
+	new_str = malloc(s1_len + n + 1);
+	if (new_str == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
-		str[i] = s1[i];
+	for (i = 0; i < s1_len; i++)
+		new_str[i] = s1[i];
 
-	for (i = 0; i < n; i++)
-		str[len1 + i] = s2[i];
+	for (j = 0; j < n; j++)
+		new_str[i + j] = s2[j];
 
-	str[len1 + n] = '\0';
+	new_str[i + j] = '\0';
 
-	return (str);
+	return (new_str);
 }
 
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,27 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _calloc - allocates memory for an array given number of elements and size
- * @nmemb: number of elements
- * @size: size of each element
- * Return: Null if error, else pointer to allocated memory
+ * _calloc - Allocates memory for an array using malloc
+ * @nmemb: Number of elements to allocate memory for
+ * @size: Size in bytes of each element
+ *
+ * Return: Pointer to allocated memory or NULL on failure
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
-	unsigned int i;
+	void *ptr;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+	ptr = malloc(nmemb * size);
 
-	return (p);
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
+
