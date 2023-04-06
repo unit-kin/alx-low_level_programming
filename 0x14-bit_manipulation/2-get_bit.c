31ad68bd1b5475ed7e2f85e86f5bf3d211636fb3
unit-kin <akkangara8@gmail.com>
2023-04-06 09:23:50 -0700

    0x14-bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,19 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
+
 /**
- * get_bit - get the value of a bit at a given index
- * @n: number to evaluate
- * @index: index starting from 0, of the bit we want to get
- * Return: Value of bit at index, or -1 if error
+ * get_bit - returns the value of a bit at a given index
+ * @n: the number to evaluate
+ * @index: the index of the bit to get, starting from 0
+ *
+ * Return: the value of the bit at index, or -1 if an error occurs
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int hold;
+        unsigned long int mask;
 
-	if (index > 64)
-		return (-1);
+        if (index >= (sizeof(unsigned long int) * 8))
+                return (-1);
 
-	hold = n >> index;
-
-	return (hold & 1);
+        mask = 1UL << index;
+        return ((n & mask) ? 1 : 0);
 }
+
