7d4bbdbe51788ae6ffd1a69d6aa44dcb45aae7d2
unit-kin <akkangara8@gmail.com>
2023-05-04 12:22:27 -0700

    updates

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,12 +10,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-        unsigned long int mask;
+	unsigned long int mask;
 
-        if (index >= (sizeof(unsigned long int) * 8))
-                return (-1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-        mask = 1UL << index;
-        return ((n & mask) ? 1 : 0);
+	mask = 1UL << index;
+	return ((n & mask) ? 1 : 0);
 }
 
